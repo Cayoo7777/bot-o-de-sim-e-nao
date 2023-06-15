eb1d0fa6153f38e715468397d438ff31d7f3ad6d
Cayo Nunes <caiiio6666@gmail.com>
2023-06-15 20:51:10 -0300

    primeiro commit

---
+++ scrypit.js
@@ -0,0 +1,19 @@
+function sim(){
+    alert("você aceitou oque eu pedi otaro");
+}
+
+function desvia(){
+
+    var desviar = document.getElementById("btnnao");
+
+        desviar.style.position = 'absolute';
+        desviar.style.bottom = geraPosicao(10, 90);
+        desviar.style.left = geraPosicao(10, 90);
+        
+    console.log('opa, desviei...');
+    
+}
+
+function geraPosicao(min, max) {
+    return (Math.random() * (max - min) + min) + "%";
+}
